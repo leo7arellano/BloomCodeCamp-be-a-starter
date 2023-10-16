848a46aa48da09330038abf0e51960d3e7fccd29
Leo Arellano <arellanoleonardo1996@gmail.com>
2023-10-16 17:01:29 -0400

    entities completed

---
+++ src/main/java/com/hcc/entities/Assignment.java
@@ -0,0 +1,14 @@
+package com.hcc.entities;
+
+public class Assignment {
+
+    private Long id;
+    private String status;
+    private Integer number;
+    private String githubUrl;
+    private String branch;
+    private String reviewVideoUrl;
+    private User user;
+    private User codeReviewer;
+
+}
---
+++ src/main/java/com/hcc/entities/Authority.java
@@ -0,0 +1,9 @@
+package com.hcc.entities;
+
+public class Authority {
+
+    private Long id;
+    private String authority;
+    private String user;
+
+}
