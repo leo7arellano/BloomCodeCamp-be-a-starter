5f84c4df7f6a58a1c71491aedfe2a82bfa0d9abb
Leo Arellano <arellanoleonardo1996@gmail.com>
2023-10-23 10:45:55 -0400

    just missing jwtutils error

--- src/main/java/com/hcc/entities/User.java
+++ src/main/java/com/hcc/entities/User.java
@@ -1,15 +1,61 @@
 package com.hcc.entities;
 
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User {
+import java.util.*;
+
+public class User implements UserDetails {
 
     private Long id;
     private Date cohortStartDate;
     private String username;
     private String password;
     private List<Authority> authorities;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+//        roles.add(new Authority("role_student"));
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
---
+++ src/main/java/com/hcc/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.hcc.repositories;
+
+import com.hcc.entities.User;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public class UserRepository {
+
+
+    public Optional<User> findByUsername(String user) {
+        return Optional.empty();
+    }
+
+
+}
